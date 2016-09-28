bd8e0a3850a8a8517c2eb416a8658da80f2bcd34
Zuolar <yam8511@gmail.com>
2016-09-28 19:07:09 +0800

    update 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.main')
+@extends('layouts.plant')
 @section('title', '404')
 
 @section('content')
-<img src="{{ url('/404.jpg') }}" width="100%" height="100%">
+<a href="{{ url('/') }}"><img src="{{ url('/404.jpg') }}" width="100%" height="100%"></a>
 @endsection
